7deb74ca291c6fd854f9c31095ebdc8f192a806d
adesha <adeshaholagodu@gmail.com>
2023-11-05 12:17:38 +0530

    changed app.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJx7fx4HPZ5RdDoThsl8DLXGNVCtPZMbo",
+    authDomain: "clone-ba81f.firebaseapp.com",
+    projectId: "clone-ba81f",
+    storageBucket: "clone-ba81f.appspot.com",
+    messagingSenderId: "39803806743",
+    appId: "1:39803806743:web:2b14771b66c185a9f96852",
+    measurementId: "G-PH764DN4PB"
+  };
+  
