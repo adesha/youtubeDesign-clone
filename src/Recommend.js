158a2859e8aa9b55c61f798a427175bdc0d4ebc4
adesha <adeshaholagodu@gmail.com>
2023-11-08 19:28:12 +0530

    Home screen completed

--- src/Recommend.js
+++ src/Recommend.js
@@ -1,9 +1,126 @@
 import React from 'react'
 import './Recommend.css'
+import VideoCard from './VideoCard'
+import profilePhoto from './images/Adesha.jpg'
+
 function Recommend() {
   return (
     <div className='recommend'>
-      <h1>Hello</h1>
+      <h2>Recommended</h2>
+      <div className='recommend-video'>
+        <VideoCard 
+        title="Become a Web Developer in a 30 days"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+        <VideoCard 
+        title="Become a Web Developer"
+        views="2.3M views"
+        timestamp="3 days ago"
+        channelImage={profilePhoto}
+        image={profilePhoto}
+        channel="Adesha"
+        />
+      </div>
     </div>
   )
 }
