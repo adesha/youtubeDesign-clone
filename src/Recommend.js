59bfe4ea8fb226d577f7e2ed687d6d17a85b1f17
adesha <adeshaholagodu@gmail.com>
2023-11-09 19:20:33 +0530

    completed full design

--- src/Recommend.js
+++ src/Recommend.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import './Recommend.css'
 import VideoCard from './VideoCard'
-import profilePhoto from './images/Adesha.jpg'
+import Attractive from './images/Attractive.jpg'
+import Video from './images/Video.jpeg'
+import Puppy from './images/PuppyTraining.jpg'
+import views from './images/Views.jpg'
+import youtube from './images/youtubeLogo.jpg'
+import pic1 from './images/User-Profile-PNG-Image.png'
+import pic2 from './images/happy-young-cute-illustration-face-profile-png.webp'
+import pic3 from './images/png-transparent-computer-icons-user-profile-male-avatar-avatar-heroes-logo-black-thumbnail.png'
+import pic4 from './images/images (1).jpeg'
+import pic5 from './images/images.jpeg'
+import pic6 from './images/images.png'
 
 function Recommend() {
   return (
@@ -9,116 +19,124 @@ function Recommend() {
       <h2>Recommended</h2>
       <div className='recommend-video'>
         <VideoCard 
-        title="Become a Web Developer in a 30 days"
+        title="How to make a good thumbnail for YOUTUBE"
         views="2.3M views"
         timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        channelImage={pic1}
+        image={Attractive}
+        channel="Mark"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to make YouTube VIDEO!!!"
+        views="3M views"
+        timestamp="20 days ago"
+        channelImage={pic2}
+        image={Video}
+        channel="Kevin"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to Train the PUPPY"
+        views="12.5M views"
+        timestamp="6 days ago"
+        channelImage={pic3}
+        image={Puppy}
+        channel="Sarah"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to get views on YOUTUBE"
+        views="1.1M views"
+        timestamp="4 days ago"
+        channelImage={pic4}
+        image={views}
+        channel="Ana"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How did youtube born"
+        views="0.3M views"
+        timestamp="4 days ago"
+        channelImage={pic5}
+        image={youtube}
+        channel="Youtube"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to Train the PUPPY"
+        views="0.8M views"
+        timestamp="6 days ago"
+        channelImage={pic6}
+        image={Puppy}
+        channel="Tanya"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How did youtube born"
+        views="1.9M views"
+        timestamp="2 days ago"
+        channelImage={pic1}
+        image={youtube}
+        channel="Google"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to make YouTube VIDEO!!!"
+        views="18.5M views"
+        timestamp="45 days ago"
+        channelImage={pic2}
+        image={Video}
+        channel="Beast"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to make a good thumbnail for YOUTUBE"
+        views="7.2M views"
+        timestamp="7 days ago"
+        channelImage={pic4}
+        image={Attractive}
+        channel="Chandler"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to get views on YOUTUBE"
+        views="0.1M views"
+        timestamp="15 days ago"
+        channelImage={pic6}
+        image={views}
+        channel="David"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to make a good thumbnail for YOUTUBE"
+        views="6.4M views"
+        timestamp="7 days ago"
+        channelImage={pic5}
+        image={Attractive}
+        channel="Mike"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
-        timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        title="How to get views on YOUTUBE"
+        views="4.0M views"
+        timestamp="10 days ago"
+        channelImage={pic4}
+        image={views}
+        channel="Hacker"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
+        title="How to Train the PUPPY"
+        views="1.7M views"
         timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        channelImage={pic3}
+        image={Puppy}
+        channel="Olga"
         />
         <VideoCard 
-        title="Become a Web Developer"
-        views="2.3M views"
+        title="How did youtube born"
+        views="0.5M views"
+        timestamp="2 days ago"
+        channelImage={pic2}
+        image={youtube}
+        channel="Edge"
+        />
+        <VideoCard 
+        title="How to make YouTube VIDEO!!!"
+        views="1.6M views"
         timestamp="3 days ago"
-        channelImage={profilePhoto}
-        image={profilePhoto}
-        channel="Adesha"
+        channelImage={pic1}
+        image={Video}
+        channel="Anthony"
         />
       </div>
     </div>
